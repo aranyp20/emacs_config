966d45c4d39f36701691533479c4cef0eb308e37
Peter Arany <peter.arany@shapr3d.com>
2026-04-12 20:01:16 +0200

    Function def color

--- style.el
+++ style.el
@@ -8,7 +8,7 @@
 ;; (set-face-attribute 'hl-line nil :background "#5D3FD3")
 
 (set-face-attribute 'font-lock-keyword-face nil :foreground "#74FAFC")
-(set-face-attribute 'font-lock-function-name-face nil :foreground "#FF2E00")
+(set-face-attribute 'font-lock-function-name-face nil :foreground "yellow")
 (set-face-attribute 'font-lock-function-call-face nil :foreground "#80FB4C")
 (set-face-attribute 'font-lock-variable-use-face nil :foreground "white")
 (set-face-attribute 'font-lock-variable-name-face nil :foreground "white")
