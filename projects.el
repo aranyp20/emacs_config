0357cc44cd578f8c6e3f00f7d86c670bff0deb5f
Peter Arany <peter.arany@shapr3d.com>
2026-05-24 16:12:53 +0200

    run and stop tests

--- projects.el
+++ projects.el
@@ -21,5 +21,17 @@
   (when-let ((win (get-buffer-window "*compilation*")))
     (delete-window win)))
 
+(defun my/build-and-run-neumann-tests ()
+  (interactive)
+  (let* ((buf (get-buffer "*compilation*"))
+         (proc (and buf (get-buffer-process buf))))
+    (if (and proc (process-live-p proc))
+        (kill-process proc)
+      (let ((default-directory (expand-file-name "~/research/metal-sandbox/")))
+        (compile
+         (concat "xcodebuild test -project build/xcode/research.xcodeproj"
+                 " -scheme NeumannTests -configuration Release 2>&1"))))))
+
 (with-eval-after-load 'evil
-  (define-key evil-normal-state-map (kbd "SPC b") 'my/build-and-run-metal-sandbox))
+  (define-key evil-normal-state-map (kbd "SPC b") 'my/build-and-run-metal-sandbox)
+  (define-key evil-normal-state-map (kbd "SPC u") 'my/build-and-run-neumann-tests))
